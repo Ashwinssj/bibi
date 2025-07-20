28567be30693009f9f2a4005ee8633c9f92c05ff
Ashwin Benke <148850788+Ashwinssj@users.noreply.github.com>
2025-07-20 23:03:43 +0530

    changes

--- bibliographer_project/settings.py
+++ bibliographer_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR=Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-_zpdo2j0bm7rh3$3(80y8kyx4w!a(ea=gx%bpurrd%^)5n3d4#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG','True')=='True'
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
