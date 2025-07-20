3da26f1dd1c729b0bf0cbc0a9e3453c988a52579
Ashwin Benke <148850788+Ashwinssj@users.noreply.github.com>
2025-07-20 22:00:10 +0530

    changes

--- bibliographer_project/wsgi.py
+++ bibliographer_project/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise # Import WhiteNoise
+from whitenoise.middleware import WhiteNoise as DjangoWhiteNoise # Import WhiteNoise (renamed for compatibility)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bibliographer_project.settings")
 
